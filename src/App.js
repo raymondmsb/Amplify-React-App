b606bf2a67776f349c7aca6fa8efbe1482162714
Ray Bach <raymondmsbach@gmail.com>
2024-05-09 18:37:46 -0500

    Fixing authenticator mistake

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 
 function App() {
   return (
-    <AmplifyAuthenticator >
+    <Authenticator >
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Hello World!</h1>
         </header>
       </div>
-    </AmplifyAuthenticator>
+    </Authenticator>
   );
 }
 
